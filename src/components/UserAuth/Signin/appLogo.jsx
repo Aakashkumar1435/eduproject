34410e31665c43ff3a47651c162fd16297eee22f
Aakash Kumar <ak4351770@gmail.com>
2025-04-19 18:24:43 +0500

    Up to date with hanzla's work

--- src/components/UserAuth/Signin/appLogo.jsx
+++ src/components/UserAuth/Signin/appLogo.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from '../../ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function AppLogo() {
     return (
         <div className="flex flex-col items-center h-48 pt-10"> {/* Adjust height here */}
             <Avatar className="w-32 h-32">
-                <AvatarImage alt="App Logo" src="/images/MyPhoto.jpg" />
+                <AvatarImage alt="App Logo" src="/hanzla.jpeg" />
                 <AvatarFallback className="text-2xl">Logo</AvatarFallback>
             </Avatar>
         </div>
